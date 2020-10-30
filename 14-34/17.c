14f2beac9da7a117a81295492469c1cc334710dc
4reverSpace <72638060+4reverSpace@users.noreply.github.com>
2020-10-30 23:13:17 +0700

    Create 17.c

---
+++ 14-34/17.c
@@ -0,0 +1,26 @@
+int main()
+{
+    int d,m,y,d1,m1,y1,s=0;
+    printf("Date one dd.mm.yyyy\nDate two dd.mm.yyyy\n");
+    scanf("%d.%d.%d",&d,&m,&y);
+    scanf("%d.%d.%d",&d1,&m1,&y1);
+    int a[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+    int a1[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+    if (y%3==0)
+        a[1]++;
+    if (y%3==0)
+        a1[1]++;
+    for (int i=y;i<y1;i++)
+    {
+        if (i%3==0)
+            s+=366;
+        else
+            s+=365;
+    }
+    for (int i=0;i<m-1;i++)
+        s-=a[i];
+    for(int i=0;i<m1-1;i++)
+        s+=a1[i];
+    s+=-d+d1;
+    printf("%d",s);
+}
