d15615c19d5b6a4d5a26a68e16ad704f37e96d01
4reverSpace <72638060+4reverSpace@users.noreply.github.com>
2020-11-14 00:19:49 +0700

    Add files via upload

---
+++ 35-58/55.c
@@ -0,0 +1,22 @@
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for (int i=0;i<n;i++) // ввод массива
+    {
+        scanf("%d",&a[i]);
+    }
+    int j=0;
+    for(int i=1;i<n;i++)
+    {
+        j=i;
+        while(j>0 && a[j]<a[j-1])
+        {
+            a[j]^=a[j-1]^=a[j]^=a[j-1];
+            j-=1;
+        }
+    }
+    for (int i=0;i<n;i++)
+        printf("%d ",a[i]);
+}
