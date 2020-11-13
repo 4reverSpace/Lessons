d11bfdc14240795b7cdedd108c632c62c8d042af
4reverSpace <72638060+4reverSpace@users.noreply.github.com>
2020-11-14 00:13:37 +0700

    Update 50.c

--- 35-58/50.c
+++ 35-58/50.c
@@ -3,14 +3,14 @@ int main()
     int n;
     scanf("%d",&n);
     int a[n];
-    for (int i=0;i<n;i++) // ввод первого массива
+    for (int i=0;i<n;i++) 
     {
         scanf("%d",&a[i]);
     }
     int m;
     scanf("%d",&m);
     int b[m];
-    for (int i=0;i<m;i++) // ввод второго массива
+    for (int i=0;i<m;i++) 
     {
         scanf("%d",&b[i]);
     }
