7d1027ce951f71c794a36d7a1e79310db8739d56
4reverSpace <72638060+4reverSpace@users.noreply.github.com>
2020-11-14 00:12:22 +0700

    Update 44.c

--- 35-58/44.c
+++ 35-58/44.c
@@ -3,6 +3,6 @@ int main()
     int a[100];
     for (int i=1;i<=100;i++)
         a[i-1]=2*i;
-    for (int i=0;i<100;i++) //вывод массива
+    for (int i=0;i<100;i++) 
         printf("%d\n" ,a[i]);
 }
