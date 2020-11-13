42ad1915381030f540067b2482b95ba8e871b072
4reverSpace <72638060+4reverSpace@users.noreply.github.com>
2020-11-14 00:13:03 +0700

    Update 47.c

--- 35-58/47.c
+++ 35-58/47.c
@@ -1,14 +1,14 @@
 int main()
 {
     int n=0;
-    scanf("%d",&n); // кол-во элементов в массиве
+    scanf("%d",&n); 
     if (n<0)
     {
         printf("Error");
         return(0);
     }
     int a[n];
-    for (int i=0;i<n;i++) //ввод элементов в массиве
+    for (int i=0;i<n;i++) 
         scanf("%d",&a[i]);
     int min=a[0];
     int max=a[0];
