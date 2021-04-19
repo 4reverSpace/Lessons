7745ef150d4e3c75ea889d7a15270d46cba3c2e0
4reverSpace <72638060+4reverSpace@users.noreply.github.com>
2021-04-19 15:48:19 +0700

    Update a5.py

--- a5/a5.py
+++ a5/a5.py
@@ -1,7 +1,7 @@
 import random
 
 
-def to_text(text):
+def to_text(text): # Перевод двумерного массив из бит в текст
     out = ''
     char = 0
     for i in range(len(text)):
@@ -12,19 +12,19 @@ def to_text(text):
     return out
 
 
-def create_key():
+def create_key(): # Создание псевдослучайного ключа
     return ''.join(str(random.randint(0, 1)) for i in range(64))
 
 
-def xor(text, key):
+def xor(text, key): # Функция ксора каждого бита еста с каждым битом ключа
     output = ''
     for i in range(len(text) * 7):
         for j in range(len(key)):
-            text[i // 7][i % 7] = text[i // 7][i % 7] ^ key[j]  ## вот тут подправить , также исправить вывод
+            text[i // 7][i % 7] = text[i // 7][i % 7] ^ key[j] 
     return text
 
 
-def do_that_thing(key):
+def do_that_thing(key): #  вот эта функция , которая изменяет ключ
     lfsr_1 = key[0:19]
     lfsr_2 = key[19:42]
     lfsr_3 = key[42:]
@@ -39,23 +39,31 @@ def do_that_thing(key):
     return lfsr_1 + lfsr_2 + lfsr_3
 
 
-class message:
+class message: # решил сделать класс ( зашифорванное сообщение , ключ и дешифорванное - параметры класса ) 
 
     def __init__(self, text1, text2, key):
         self.dec_text = text1
         self.enc_text = text2
         self.key = do_that_thing(list(map(int, list(str(key)))))
 
-    def decrypt(self):  ## могут возникнуть служебные символы, поэтому декодированный текст всегда в чистых битах
+    def decrypt(self):  ## могут возникнуть служебные символы, поэтому декодированный текст всегда в чистых битах(поправочка , по 7 бит, askii коды всех стандартных символов умещаются  ) 
         temp=[]
         for i in range(len(self.enc_text)//7):
-            temp.append([int(self.enc_text[i*7+j]) for j in range(7)])
+            temp.append([int(self.enc_text[i*7+j]) for j in range(7)]) 
         self.dec_text = to_text(xor(temp, self.key))
 
-    def encrypt(self):
+    def encrypt(self): #функция шифорвки 
         message = []
         for i in self.dec_text:
             message.append(list((map(int, list(format(ord(i), '07b'))))))
         temp = xor(message, self.key)
         for i in temp:
             self.enc_text = self.enc_text + ''.join(map(str,i))
+# Примеры работы:
+#Дешифрованное сообщение :test
+#Ключ:
+# Шифорванное сообщение:1110100110010111100111110100
+
+# Шифорванное сообещние:1110100110010111100111110100
+# Ключ:1101000110001110001110110111011110101000111001110000010100010010
+# Дешифорванное сообщение :test
